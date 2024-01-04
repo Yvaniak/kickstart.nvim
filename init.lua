289f06c5eda804efa00fb5e71e40c988b6d5ca50
Yvaniak <67830211+Yvaniak@users.noreply.github.com>
2024-01-04 17:18:55 +0100

    add Neotree plugin

--- init.lua
+++ init.lua
@@ -70,6 +70,16 @@ require('lazy').setup({
   --my setup
   "christoomey/vim-tmux-navigator",
 
+  {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      },
+  },
 
   -- NOTE: First, some plugins that don't require any configuration
 
