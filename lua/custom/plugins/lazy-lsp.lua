b3e181be23d976440c7f7c2855e31b46b28fa099
Yvaniak <yvaniak11@gmail.com>
2024-10-03 00:44:24 +0200

    fmt

--- lua/custom/plugins/lazy-lsp.lua
+++ lua/custom/plugins/lazy-lsp.lua
@@ -1,8 +1,8 @@
 return {
-"dundalek/lazy-lsp.nvim",
+  "dundalek/lazy-lsp.nvim",
   dependencies = {
     "neovim/nvim-lspconfig",
-    {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/nvim-cmp",
   },
